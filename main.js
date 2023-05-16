d93b3ed9d07e4110863851143996b6cc7af1e27e
Mark Prettyman <markiesmyname@Marks-iMac.local>
2023-05-16 10:02:52 -0700

    simple liker

--- main.js
+++ main.js
@@ -4,6 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorModal = document.querySelector('#modal');
+const hearts = document.querySelectorAll('.like-glyph');
+
+errorModal.classList.add('hidden')
+
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    if(heart.textContent === EMPTY_HEART) {
+      mimicServerCall()
+      .then(() => {
+        heart.textContent = FULL_HEART
+        heart.classList.add('activated-heart')
+      })
+      .catch(error => {
+        const errorMessage = document.querySelector('#modal-message');
+        errorMessage.textContent= error;
+        errorModal.classList.remove('hidden');
+        // Hide error modal after 3 seconds
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+    } else {
+      heart.textContent = EMPTY_HEART;
+      heart.classList.remove('activated-heart');
+    }
+  });
+});
+
+
 
 
 
